e750e92b0665a24b8233f20c26b731bd3fde5b75
lukaszd <lukasz.d@bitnoi.se>
2022-08-24 16:23:13 +0200

    init

---
+++ test/characterization/north-capital/account.test.ts
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+
+describe('Configuration Test', () => {
+    it('check engine', () => {
+        const testFlag = true
+        expect(testFlag).to.be.true;
+    })
+});
