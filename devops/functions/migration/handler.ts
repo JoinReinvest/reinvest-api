723c909cd5f22b01fae77a0ee459e816aecd076a
Lukas Duraj <lukasz@hookooekoo.co>
2023-03-02 11:13:04 +0100

    feat: migration down

--- devops/functions/migration/handler.ts
+++ devops/functions/migration/handler.ts
@@ -1,15 +1,16 @@
 import {migrate} from "Reinvest/migration";
 
 export const main = async (event: any, context: any, callback: any) => {
+    const migrationType = event === 'down' ? 'migrateDown' : 'migrateLatest';
     try {
-        console.log('Starting migration');
-        await migrate('migrateLatest')
+        console.log('Starting migration: ' + migrationType);
+        await migrate(migrationType);
         console.log('Migration finished!');
     } catch (error: any) {
-        callback(error, event);
+        callback(error, {event, migrationType, status: false});
         console.log({error});
         return;
     }
 
-    callback(null, event);
+    callback(null, {event, migrationType, status: true});
 };
