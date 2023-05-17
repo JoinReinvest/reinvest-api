b56b5191affa47ae28e9f0864419a6f7c981aa10
Lukas Duraj <lukasz@hookooekoo.co>
2023-05-17 09:59:58 +0200

    feat: add investments table

--- Reinvest/Investments/src/Infrastructure/Providers/AdaptersProviders.ts
+++ Reinvest/Investments/src/Infrastructure/Providers/AdaptersProviders.ts
@@ -1,9 +1,13 @@
 import { ContainerInterface } from 'Container/Container';
+import { Investments } from 'Investments/index';
+import {
+  createInvestmentsDatabaseAdapterProvider,
+  InvestmentsDatabaseAdapterInstanceProvider,
+} from 'Investments/Infrastructure/Adapters/PostgreSQL/DatabaseAdapter';
+import { InvestmentsRepository } from 'Investments/Infrastructure/Adapters/Repository/InvestmentsRepository';
 import { QueueSender } from 'shared/hkek-sqs/QueueSender';
 import { SimpleEventBus } from 'SimpleAggregator/EventBus/EventBus';
 import { SendToQueueEventHandler } from 'SimpleAggregator/EventBus/SendToQueueEventHandler';
-
-import { Investments } from '../..';
 
 export default class AdaptersProviders {
   private config: Investments.Config;
@@ -17,5 +21,10 @@ export default class AdaptersProviders {
       .addAsValue(SimpleEventBus.getClassName(), new SimpleEventBus(container))
       .addObjectFactory(QueueSender, () => new QueueSender(this.config.queue), [])
       .addObjectFactory(SendToQueueEventHandler, (queueSender: QueueSender) => new SendToQueueEventHandler(queueSender), [QueueSender]);
+
+    // database
+    container
+      .addAsValue(InvestmentsDatabaseAdapterInstanceProvider, createInvestmentsDatabaseAdapterProvider(this.config.database))
+      .addSingleton(InvestmentsRepository, [InvestmentsDatabaseAdapterInstanceProvider]);
   }
 }
