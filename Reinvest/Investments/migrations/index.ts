b5e26403e5c2252298146f5637ce75a6ada3044e
Lukas Duraj <lukasz@hookooekoo.co>
2023-05-23 14:37:04 +0200

    fix: move transaction in time

--- Reinvest/Investments/migrations/index.ts
+++ Reinvest/Investments/migrations/index.ts
@@ -1,2 +1,2 @@
 export * as migration_20230517094420 from './investments_1_create_investment';
-export * as migration_20230517134500_create_transaction from './investments_2_create_transaction';
+export * as migration_20230523143500_create_transaction from './investments_2_create_transaction';
--- Reinvest/Trading/migrations/index.ts
+++ Reinvest/Trading/migrations/index.ts
@@ -1,1 +1,1 @@
-export * as migration_20230522123100_create_trades from './trading_1_create_trades';
+export * as migration_20230523143501_create_trades from './trading_1_create_trades';
