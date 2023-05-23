a57a00e1eb02347572c63e2eae4d14dc0b7dc259
Bartosz Burdalski <bartosz.b@bitnoi.se>
2023-05-23 13:25:18 +0200

    feat: Delete old copied methods

--- Reinvest/Investments/src/Infrastructure/Adapters/Repository/FeesRepository.ts
+++ Reinvest/Investments/src/Infrastructure/Adapters/Repository/FeesRepository.ts
@@ -1,7 +1,4 @@
-import { InvestmentsDatabaseAdapterProvider, investmentsTable } from 'Investments/Infrastructure/Adapters/PostgreSQL/DatabaseAdapter';
-import type { InvestmentCreate } from 'Investments/Infrastructure/UseCases/CreateInvestment';
-import { Investment } from 'Investments/Infrastructure/ValueObject/Investment';
-import type { Money } from 'Money/Money';
+import { InvestmentsDatabaseAdapterProvider } from 'Investments/Infrastructure/Adapters/PostgreSQL/DatabaseAdapter';
 
 export class FeesRepository {
   public static getClassName = (): string => 'FeesRepository';
@@ -10,60 +7,5 @@ export class FeesRepository {
 
   constructor(databaseAdapterProvider: InvestmentsDatabaseAdapterProvider) {
     this.databaseAdapterProvider = databaseAdapterProvider;
-  }
-
-  async get(investmentId: string) {
-    const investment = await this.databaseAdapterProvider
-      .provide()
-      .selectFrom(investmentsTable)
-      .select([
-        'accountId',
-        'amount',
-        'bankAccountId',
-        'dateCreated',
-        'dateUpdated',
-        'id',
-        'profileId',
-        'recurringInvestmentId',
-        'scheduledBy',
-        'status',
-        'subscriptionAgreementId',
-      ])
-      .where('id', '=', investmentId)
-      .executeTakeFirst();
-
-    if (!investment) return false;
-
-    return Investment.create(investment);
-  }
-
-  async create(investment: InvestmentCreate, money: Money) {
-    const { id, profileId, accountId, bankAccountId, scheduledBy, status } = investment;
-    const amount = money.getAmount();
-    try {
-      await this.databaseAdapterProvider
-        .provide()
-        .insertInto(investmentsTable)
-        .values({
-          id,
-          profileId,
-          amount,
-          accountId,
-          bankAccountId,
-          dateCreated: new Date(),
-          dateUpdated: new Date(),
-          subscriptionAgreementId: null,
-          scheduledBy,
-          recurringInvestmentId: null,
-          status,
-        })
-        .execute();
-
-      return true;
-    } catch (error: any) {
-      console.error(`Cannot create investment: ${error.message}`, error);
-
-      return false;
-    }
   }
 }
