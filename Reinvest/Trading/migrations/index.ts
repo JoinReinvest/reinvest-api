b958ee6d1090b6a64dd512f17318f9900114bae4
Lukas Duraj <lukasz@hookooekoo.co>
2023-05-25 07:50:41 +0200

    fix: reorder migrations

--- Reinvest/Trading/migrations/index.ts
+++ Reinvest/Trading/migrations/index.ts
@@ -1,2 +1,2 @@
 export * as migration_20230523143501_create_trades from './trading_1_create_trades';
-export * as migration_20230524150200_alter_trades from './trading_2_add_new_trades_columns';
+export * as migration_20230524110600_alter_trades from './trading_2_add_new_trades_columns';
