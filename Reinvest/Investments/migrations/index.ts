9f917f667ff54e28f73cc0a545c35f58a8476c75
Lukas Duraj <lukasz@hookooekoo.co>
2023-05-26 12:55:54 +0200

    fix: reorder migrations

--- Reinvest/Investments/migrations/index.ts
+++ Reinvest/Investments/migrations/index.ts
@@ -4,4 +4,4 @@ export * as migration_20230522162010_subscription_agreement from './investments_
 export * as migration_20230523143500_create_transaction from './investments_4_create_transaction';
 export * as migration_20230524104015_create_fees from './investments_5_fees';
 export * as migration_20230524150120_add_tradeId_and_startedDate from './investments_6_tradeId_and_startedDate';
-export * as migration_20230525111210_portfolio_id from './investments_7_portfolio_id';
+export * as migration_20230525161210_portfolio_id from './investments_7_portfolio_id';
