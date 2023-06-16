869bd2f2be5c438db17c38d5d0d393965866e161
Lukas Duraj <lukasz@hookooekoo.co>
2023-06-16 19:19:36 +0200

    chore: changelog

--- Reinvest/Withdrawals/migrations/index.ts
+++ Reinvest/Withdrawals/migrations/index.ts
@@ -1,1 +1,1 @@
-export * as migration_20230616170100_create_dividends_requests from './withdrawals_x_create_dividends_requests';
+export * as migration_20230616170100_create_dividends_requests from './withdrawals_create_dividends_requests';
