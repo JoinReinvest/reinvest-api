7da5a7d3ee83b3b0de9d4524658287d5348f0c7f
Lukas Duraj <lukasz@hookooekoo.co>
2023-06-23 10:23:04 +0200

    refactor: reorder migrations because of integration

--- Reinvest/Identity/migrations/index.ts
+++ Reinvest/Identity/migrations/index.ts
@@ -1,3 +1,3 @@
 export * as migration_20230215143400 from './identity_1_create_user';
 export * as migration_20230220123200 from './identity_2_create_phone_verification';
-export * as migration_20230621183900_add_banned_ids from './identity_3_add_banned';
+export * as migration_20230623102101_add_banned_ids from './identity_3_add_banned';
