88ca958cb20eb971762fcdca8011e67a86bdc38b
Lukas Duraj <lukasz@hookooekoo.co>
2023-06-30 14:09:41 +0200

    feat: integration test

--- devops/functions/firebase/handler.ts
+++ devops/functions/firebase/handler.ts
@@ -1,28 +1,29 @@
-import { SQSEvent, SQSHandler, SQSRecord } from 'aws-lambda'
-import { credential, initializeApp, messaging } from 'firebase-admin'
-import { FIREBASE_SERVICE_ACCOUNT_JSON } from 'Reinvest/config'
+import { SQSEvent, SQSHandler, SQSRecord } from 'aws-lambda';
+import admin from 'firebase-admin';
+import { FIREBASE_SERVICE_ACCOUNT_JSON } from 'Reinvest/config';
 
 export const main: SQSHandler = async (event: SQSEvent) => {
-    const record = event.Records.pop() as SQSRecord
+  const record = event.Records.pop() as SQSRecord;
 
-    try {
-        const {
-            data: { token, title, body },
-        } = JSON.parse(record.body)
-        initializeApp({
-            credential: credential.cert(FIREBASE_SERVICE_ACCOUNT_JSON),
-        })
+  try {
+    const {
+      data: { token, title, body },
+    } = JSON.parse(record.body);
 
-        const message = {
-            token,
-            notification: {
-                title,
-                body,
-            },
-        }
+    admin.initializeApp({
+      credential: admin.credential.cert(<admin.ServiceAccount>JSON.parse(FIREBASE_SERVICE_ACCOUNT_JSON)),
+    });
 
-        await messaging().send(message)
-    } catch (error: any) {
-        console.log(error)
-    }
-}
+    const message = {
+      token,
+      notification: {
+        title,
+        body,
+      },
+    };
+
+    await admin.messaging().send(message);
+  } catch (error: any) {
+    console.log(error);
+  }
+};
