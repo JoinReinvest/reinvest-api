d65eb96c0a093a506d769037a640b6a7df7219d6
Lukas Duraj <lukasz@hookooekoo.co>
2023-07-18 12:54:28 +0200

    refactor: update restore procedure

--- devops/local/restoreSnapshot.sh
+++ devops/local/restoreSnapshot.sh
@@ -3,13 +3,12 @@
 echo "Database Snapshot Restore Procedure:"
 echo "0. Warning! This is a manual database recovery procedure. It is not automated and it is not part of the CI/CD pipeline."
 echo "1. Copy file '.env.saas.dist' into '.env.$NODE_ENV' and use the same values as you use in the Github Actions variables for the $NODE_ENV environment"
-echo "2. Go to the 'Amazon RDS/Snapshots/System' and find the latest snapshot for the database 'reinvest-$NODE_ENV-postgresql'"
+echo "2. Go to the 'Amazon RDS/Snapshots/System' and find the latest snapshot for the database 'reinvest-$NODE_ENV-<randomized-string>'"
 echo "3. Copy the snapshot ARN"
 echo "4. Enter the ARN below and click 'Enter'"
 echo "5. Wait for the database to be restored"
-echo "6. Go to the 'AWS/Cloudformation/Stacks/reinvest-${NODE_ENV}/Outputs. Find 'DatabaseHost' variable. Copy the value and replace Github Actions '${NODE_ENV}_POSTGRESQL_HOST' variable with it"
+echo "6. Go to the 'AWS/Cloudformation/Stacks/reinvest-${NODE_ENV}/Outputs. Find 'DatabaseHost' variable. Copy the value and replace Github Actions '${NODE_ENV}_POSTGRESQL_HOST' variable with it in Github/reinvest-api/Settings/Security/Secrets and variables/Actions"
 echo "7. Run the functions pipeline to deploy the functions with the new database host"
-echo "8. Optional - you can remove previous database instance to save costs. Go to the 'Amazon RDS/Instances' and find the previous database instance."
 
 read -p "Enter the snapshot ARN: " snapshotArn
 
@@ -21,4 +20,4 @@ fi
 echo "Provided ARN: $snapshotArn"
 POSTGRESQL_RDS_SNAPSHOT_IDENTIFIER=$snapshotArn serverless deploy -s $NODE_ENV --config serverless-saas.ts
 
-echo "Database restore procedure completed. Validate manually if the database is restored correctly. Do not forget of steps 6-8!"
+echo "Database restore procedure completed. Validate manually if the database is restored correctly. Do not forget of steps 6-7!"
