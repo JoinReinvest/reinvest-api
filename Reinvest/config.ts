0908bffe1b2e6fc2089efd542484773a9d86a1fc
Lukas Duraj <lukasz@hookooekoo.co>
2023-07-21 17:32:39 +0200

    wip: 29 seconds timeout?

--- Reinvest/config.ts
+++ Reinvest/config.ts
@@ -8,7 +8,6 @@ export const NORTH_CAPITAL_CONFIG = {
   CLIENT_ID: process.env.NORTH_CAPITAL_CLIENT_ID as string,
   DEVELOPER_API_KEY: process.env.NORTH_CAPITAL_DEVELOPER_API_KEY as string,
   API_URL: process.env.NORTH_CAPITAL_API_URL as string,
-  OFFERING_ID: process.env.NORTH_CAPITAL_OFFERING_ID as string,
 };
 
 export const VERTALO_CONFIG = {
